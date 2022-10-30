e5e07fb0a1ed40ad43b47d53cb41dbef6dea735b
Adeyemi Isaac <adeyemiisaac@gmail.com>
2022-10-30 21:45:23 +0000

    learn

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+int a, b;
+  
+printf("Enter first number: ");
+scanf("%d", &a);
+
+printf("Enter second number; ");
+scanf("%d", &b);
+
+printf("A : %d\n B : %d\n , a, b);
+
+return 0;
+}
